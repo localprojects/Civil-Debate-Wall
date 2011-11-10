c386afa88383d2159683a31f184f587a8fea7c5e
Matt Wright <matt@localprojects.net>
2011-11-10 15:45:59 -0500

    Working on frequet words display

--- cdw/static/js/app/stats.js
+++ cdw/static/js/app/stats.js
@@ -1,11 +1,11 @@
 if(window.models === undefined) {
   window.models = {}
 }
-
+/*
 models.MostDebatedList = Backbone.Collection.extend({
   
 })
-
+*/
 window.StatsScreenView = Backbone.View.extend({
   tagName: 'div',
   className: 'stats-view',
@@ -19,16 +19,22 @@ window.StatsScreenView = Backbone.View.extend({
   initialize: function() {
     $('div.stats-outer').append($(this.render().el));
     
-    this.$('div.screen').hide();
     
     var data = this.model.toJSON();
     
     window.StatsMostDebated = 
       new StatsMostDebatedView({
         el: $('div.most-debated'), 
-        model: new models.MostDebatedList(data.mostDebatedOpinions) 
+        model: new Backbone.Collection(data.mostDebatedOpinions) 
       });
       
+    window.StatsFrequentWords = 
+      new StatsFrequentWordsView({
+        el: $('div.frequent-words'),
+        model: new Backbone.Collection(data.frequentWords)
+      });
+    
+    this.$('div.screen').hide();
     this.gotoScreen('screen-1');
   },
   
@@ -145,6 +151,110 @@ window.StatsMostDebatedView = Backbone.View.extend({
     this.$currentSelection = this.$('div.menu-view li.' + this.currentSelector);
     this.$currentSelection.addClass('selected-' + yesNo)
     this.$('div.detail-view li.' + this.currentSelector).show();
+  },
+  
+});
+
+window.StatsFrequentWordsView = Backbone.View.extend({
+  
+  events: {
+    'click a.back-btn': 'showWordMenu',
+  },
+  
+  initialize: function() {
+    this.detailPosts = [];
+    this.render();  
+  },
+  
+  render: function() {
+    this.dRow = 0;
+    this.allButtons = [];
+    this.model.each(this.addWordBtn, this);
+    
+    $.each($('div.word-row'), function(index, item){
+      // Adjust the top position of each row
+      var $item = $(item);
+      $item.css('top', index * 66);
+      
+      // Adjust the width of each row to be that of its buttons
+      var width = 0;
+      $.each($('button', item), function(i, btn) {
+        width += $(btn).outerWidth();
+      });
+      $item.width(width+60);
+      
+    });
+    
+    this.$('button')
+      .click($.proxy(function(e) {
+        e.preventDefault();
+        this.showWordDetail($(e.currentTarget).data('index'));
+      }, this))
+      .mouseover($.proxy(function(e) {
+        e.preventDefault();
+        var index = $(e.currentTarget).data('index');
+        for(var i=0; i < this.allButtons.length; i++) {
+          if(index != i) {
+            this.allButtons[i].css('opacity', 0.5);
+          }
+        }
+      }, this))
+      .mouseout($.proxy(function(e) {
+        e.preventDefault();
+        for(var i=0; i < this.allButtons.length; i++) {
+          this.allButtons[i].css('opacity', 1);
+        }
+      }, this));
+      
+    this.$('div.word-detail').hide();      
+    
+    return this;
+  },
+  
+  addWordBtn: function(item, index) {
+    // Get a color based on the ratio
+    var colors = ['#68b7fd', '#5191d5', '#457ec1', '#3767a9', '#3a546c',
+                  '#3f3c4d', '#6c4434', '#8a4d29', '#c8611d', '#e0681c']
+    var cIndex = 9 - Math.round(item.get('ratio') * 9);
+    
+    // Create button
+    var $btn = $('<button class="word-btn">' + item.get('word') + '</button>');
+    $btn.css('background-color', colors[cIndex]);
+    $btn.data('index', index);
+    
+    // Add it to the appropriate row
+    var $row = $(this.$('div.word-row')[this.dRow]);
+    $row.append($btn);
+    
+    // Increment the next row
+    this.dRow = (this.dRow == 3) ? 0 : this.dRow + 1;
+    
+    this.allButtons.push($btn);
+  },
+  
+  showWordMenu: function(e) {
+    e.preventDefault();
+    
+    _.each(this.detailPosts, function(item) {item.remove() })
+    this.detailPosts = [];
+    
+    this.$('div.word-detail').hide();
+    this.$('div.word-menu').show();
+  },
+  
+  showWordDetail: function(index) {
+    
+    this.$('div.word-menu').hide();
+    this.$('div.word-detail').show();
+    
+    var model = this.model.at(index);
+    var posts = model.get('posts');
+    
+    for(var i=0; i < posts.length; i++) {
+      var view = new ResponseItemView({ model:new Backbone.Model(posts[i]) });
+      this.detailPosts.push(view);
+      this.$('div.responses-list').append(view.render().el);
+    }
   },
   
 })
--- cdwapi/views/stats.py
+++ cdwapi/views/stats.py
@@ -4,6 +4,18 @@ from cdwapi import (jsonify, not_found_on_error, auth_token_or_logged_in_require
 from flask import request, current_app
 from flaskext.login import current_user
 
+def multikeysort(items, columns):
+    from operator import itemgetter
+    comparers = [ ((itemgetter(col[1:].strip()), -1) if col.startswith('-') else (itemgetter(col.strip()), 1)) for col in columns]  
+    def comparer(left, right):
+        for fn, mult in comparers:
+            result = cmp(fn(left), fn(right))
+            if result:
+                return mult * result
+        else:
+            return 0
+    return sorted(items, cmp=comparer)
+
 def load_views(blueprint):
     
     @blueprint.route('/stats/questions/<question_id>', methods=['GET'])
@@ -31,7 +43,7 @@ def load_views(blueprint):
             mostDebatedOpinions.append({ 
                 'id': str(thread.id), 
                 'commentCount': len(posts_in_thread),
-                 'firstPost': first_post.as_dict(),
+                'firstPost': first_post.as_dict(),
             })
             
             if first_post.yesNo == 1:
@@ -58,24 +70,25 @@ def load_views(blueprint):
         words = dict()
         
         for thread in threads:
+            first_post = thread.firstPost
             posts_in_thread = cdw.posts.with_fields(thread=thread)
-            first_post = posts_in_thread[0]
-            postId = str(first_post.id)
-            # the first post
-            #debateID = debate['_id']
-            #stance = debate['stance']
             
             # first from the debate starter
             for post in posts_in_thread:
                 for word in post.text.split():
                     if word not in words:
-                        words[word] = {'threads': [postId], 'yesCases': 0, 'noCases': 0, 'total': 0}
+                        words[word] = {
+                            'posts': [post.as_dict()], 
+                            'yesCases': 0, 
+                            'noCases': 0, 
+                            'total': 0
+                        }
                     else:
-                        words[word]['threads'].append(postId)
+                        words[word]['posts'].append(post.as_dict())
                     
                     words[word]['total'] += 1
                     
-                    if first_post.yesNo == 1:
+                    if post.yesNo == 1:
                         words[word]['yesCases'] += 1
                     else:   
                         words[word]['noCases'] += 1                             
@@ -86,11 +99,27 @@ def load_views(blueprint):
             for word in words:
                 words[word]['word'] = word
                 wordList.append(words[word])
-                    
                             
             # for every word, a list of debate threads where it appears             
-            sortedWordList = sorted(wordList, key=lambda k: k['total'])
+            sortedWordList = sorted(wordList, key=lambda k: k['total'])[0:20]
             sortedWordList.reverse() # biggest first
+            
+            # only the top 20
+            #sortedWordList = sortedWordList
+            
+            # add a ratio value
+            for item in sortedWordList:
+                ratio = float(item['yesCases']) / float(item['total'])
+                print ratio
+                item['ratio'] = ratio
+            
+            #sortedWordList = sorted(sortedWordList, key=lambda k: k['ratio'])
+            sortedWordList = sorted(sortedWordList, key=lambda k: k['ratio'])
+            sortedWordList.reverse()
+            
+            # Trying to sort on multiple keys
+            #sortedWordList = multikeysort(sortedWordList, ['-total','-ratio'])
+            
             
             # most liked debates
             likedFirstPosts = sorted(first_posts, key=lambda p: p.likes)[:5]
@@ -106,10 +135,9 @@ def load_views(blueprint):
             mostDebatedOpinions.reverse() # biggest first           
             
             # gather and return
-            
             stats['debateTotals'] = {'yes': int(yes_debate_count), 'no': int(no_debate_count)}
             stats['likeTotals'] = {'yes': int(yes_likes), 'no': int(no_likes)}              
-            stats['frequentWords'] = sortedWordList[0:20] # only the top 20
+            stats['frequentWords'] = sortedWordList
             stats['mostLikedDebates'] = liked # only the top 5
             stats['mostDebatedOpinions'] = mostDebatedOpinions[0:5] # only the top 5
             
