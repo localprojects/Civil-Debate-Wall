027c3e273e91b2ade25712a8197da9dd699b1e27
Matt Wright <matt@localprojects.net>
2011-11-22 11:21:51 -0500

    Stats fix

--- cdwapi/views/stats.py
+++ cdwapi/views/stats.py
@@ -31,7 +31,7 @@ def load_views(blueprint):
         stats['question'] = question.as_dict()
         
         threads = cdw.threads.with_fields(question=question)
-            
+        
         yes_debate_count = 0
         no_debate_count = 0
         yes_likes = 0
@@ -79,6 +79,7 @@ def load_views(blueprint):
             # first from the debate starter
             for post in posts_in_thread:
                 for word in post.text.split():
+                    
                     if word not in words:
                         words[word] = {
                             'posts': [post.as_dict()], 
@@ -87,6 +88,7 @@ def load_views(blueprint):
                             'total': 0
                         }
                     else:
+                        
                         words[word]['posts'].append(post.as_dict())
                     
                     words[word]['total'] += 1
@@ -94,53 +96,53 @@ def load_views(blueprint):
                     if post.yesNo == 1:
                         words[word]['yesCases'] += 1
                     else:   
-                        words[word]['noCases'] += 1                             
+                        words[word]['noCases'] += 1                       
             
-            # turn the dictionary into a list of objects so we can sort it
-            wordList = list()
-            
-            for word in words:
-                words[word]['word'] = word
-                wordList.append(words[word])
-                            
-            # for every word, a list of debate threads where it appears             
-            sortedWordList = sorted(wordList, key=lambda k: k['total'])[0:20]
-            sortedWordList.reverse() # biggest first
-            
-            # only the top 20
-            #sortedWordList = sortedWordList
-            
-            # add a ratio value
-            for item in sortedWordList:
-                ratio = float(item['yesCases']) / float(item['total'])
-                item['ratio'] = ratio
-            
-            #sortedWordList = sorted(sortedWordList, key=lambda k: k['ratio'])
-            sortedWordList = sorted(sortedWordList, key=lambda k: k['ratio'])
-            sortedWordList.reverse()
-            
-            # Trying to sort on multiple keys
-            #sortedWordList = multikeysort(sortedWordList, ['-total','-ratio'])
-            
-            
-            # most liked debates
-            likedFirstPosts = sorted(first_posts, key=lambda p: p.likes)[:5]
-            likedFirstPosts.reverse()                
-            
-            liked = list()
-            for post in likedFirstPosts:
-                liked.append({'id': str(post.id), 'likes': post.likes})
+        # turn the dictionary into a list of objects so we can sort it
+        wordList = list()
+        
+        for word in words:
+            words[word]['word'] = word
+            wordList.append(words[word])
+                        
+        # for every word, a list of debate threads where it appears             
+        sortedWordList = sorted(wordList, key=lambda k: k['total'])
+        sortedWordList.reverse() # biggest first
+        
+        # only the top 20
+        sortedWordList = sortedWordList[:20]
+        
+        # add a ratio value
+        for item in sortedWordList:
+            ratio = float(item['yesCases']) / float(item['total'])
+            item['ratio'] = ratio
+        
+        #sortedWordList = sorted(sortedWordList, key=lambda k: k['ratio'])
+        sortedWordList = sorted(sortedWordList, key=lambda k: k['ratio'])
+        sortedWordList.reverse()
+        
+        # Trying to sort on multiple keys
+        #sortedWordList = multikeysort(sortedWordList, ['-total','-ratio'])
+        
+        
+        # most liked debates
+        likedFirstPosts = sorted(first_posts, key=lambda p: p.likes)[:5]
+        likedFirstPosts.reverse()                
+        
+        liked = list()
+        for post in likedFirstPosts:
+            liked.append({'id': str(post.id), 'likes': post.likes})
 
-            # most debated opinions
-            # gathered in the first for loop
-            mostDebatedOpinions = sorted(mostDebatedOpinions, key=lambda k: k['commentCount'])
-            mostDebatedOpinions.reverse() # biggest first           
-            
-            # gather and return
-            stats['debateTotals'] = {'yes': int(yes_debate_count), 'no': int(no_debate_count)}
-            stats['likeTotals'] = {'yes': int(yes_likes), 'no': int(no_likes)}              
-            stats['frequentWords'] = sortedWordList
-            stats['mostLikedDebates'] = liked # only the top 5
-            stats['mostDebatedOpinions'] = mostDebatedOpinions[0:5] # only the top 5
-            
-            return jsonify(stats)   
+        # most debated opinions
+        # gathered in the first for loop
+        mostDebatedOpinions = sorted(mostDebatedOpinions, key=lambda k: k['commentCount'])
+        mostDebatedOpinions.reverse() # biggest first           
+        
+        # gather and return
+        stats['debateTotals'] = {'yes': int(yes_debate_count), 'no': int(no_debate_count)}
+        stats['likeTotals'] = {'yes': int(yes_likes), 'no': int(no_likes)}              
+        stats['frequentWords'] = sortedWordList
+        stats['mostLikedDebates'] = liked # only the top 5
+        stats['mostDebatedOpinions'] = mostDebatedOpinions[0:5] # only the top 5
+        
+        return jsonify(stats)   
