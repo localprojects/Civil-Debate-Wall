3f8f4ab8d6f8130a5a2ba88c0371a2a96e8ce10e
Matt Wright <matt@localprojects.net>
2011-11-21 19:48:59 -0500

    Added missing command registration

--- manage.py
+++ manage.py
@@ -4,9 +4,11 @@ os.environ.setdefault('FLAILS_ENV', 'script')
 import main
 from flaskext.script import Manager
 from flaskext.assets import ManageAssets
+from script.users import MakeAdmin
 
 manager = Manager(main.app)
 manager.add_command("assets", ManageAssets())
+manager.add_command("make_admin", MakeAdmin())
 
 if __name__ == "__main__":
     manager.run()
