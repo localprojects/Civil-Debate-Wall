d06aea0e771c39db6be194eb63e42b7f7c13f7af
Matt Wright <matt@localprojects.net>
2011-11-28 18:54:30 -0500

    Compiled assets

---
+++ cdw/static/profile.js
@@ -0,0 +1,3 @@
+
+window.ProfileView=Backbone.View.extend({el:$('div.profile-view'),events:{'click a.recent-activity':'onBtnClick','click a.my-debates':'onBtnClick',},initialize:function(){this.$('div.panel').hide();this.select(this.$('a.recent-activity'));},select:function($btn){if(this.$selected){this.$selected.toggleClass('selected');var selector='div.'+this.$selected.attr('class');this.$(selector).hide();}
+this.$('div.'+$btn.attr('class')).show();this.$selected=$btn;this.$selected.toggleClass('selected');},onBtnClick:function(e){e.preventDefault();this.select($(e.currentTarget));}});$(function(){tools.bodyClass('profile-index',function(){window.Profile=new ProfileView;});tools.bodyClass('profile-edit',function(){window.VerifyPhone=new VerifyPhoneView;});});
