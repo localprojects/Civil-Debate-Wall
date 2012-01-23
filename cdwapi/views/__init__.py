1862a06455d920498ef1ae7a2b20751a236d2276
Sundar Raman <cybertoast@gmail.com>
2012-01-22 23:16:37 -0500

    Changed copyright statement to explicitly state AGPL

--- cdwapi/views/__init__.py
+++ cdwapi/views/__init__.py
@@ -1,6 +1,6 @@
 """
     :copyright: (c) 2011 Local Projects, all rights reserved
-    :license: See LICENSE for more details.
+    :license: Affero GNU GPL v3, see LEGAL/LICENSE for more details.
 """
 def load_views(blueprint):
     from cdwapi.views import users, questions, posts, threads, sms, stats, utils
