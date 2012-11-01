0b5a790bd3f143522cfbe8c4b5256874834e7158
Sundar Raman <sundar@localprojects.net>
2012-11-01 19:38:27 -0400

    Add stats folder

---
+++ cdw/static/js/views/stats/stats.js
@@ -0,0 +1,130 @@
+define(['jquery', 'underscore', 'backbone', 'models/stats' , 'models/question', 'text!templates/stats/stats.html'], function ($, _, Backbone, StatsModel, QuestionModel, _statsTemplate) {
+
+    var CommentsView = Backbone.View.extend({
+
+        el: $("#stats"),
+
+        initialize: function () {
+           
+           this.models = {
+              stats : new StatsModel(),
+              question :  new QuestionModel()
+           }
+        },
+        
+       events: {
+            "click .stats-tab .btn" : "showContent",            
+            "click .debates .debate .reply" : "goThread",
+            "click .debate .desc": "goThread"
+            
+        },
+        
+        drawNum : function(statsdata) {
+           
+         
+           var totalheight = $(document).height() - $(".opinion-bar").height(),
+               totalYes = statsdata.debateTotals.yes,
+               totalNo  = statsdata.debateTotals.no,
+               yesHeight = parseInt((totalYes / (totalYes + totalYes)) * totalheight);
+               
+                $("#triangle-orange").css("border-width", "0px "+Math.floor($("#wrapper").width()/2)+"px 50px");
+                $(".opinion-bar").find(".yes").css("height", yesHeight + "px");
+               
+               
+           
+           
+           
+        },
+        
+        goThread : function(e) {           
+           e.preventDefault();
+           var fragment = ($(e.currentTarget).hasClass("desc")) ? "" : "/reply";
+           window.location.href = "comments.html#/questions/"+this.models.question.data.id+"/debates/"+$(e.currentTarget).parent().parent().parent().attr("data-did")+"/posts" + fragment;
+        },
+        
+        
+        showContent : function(e) {
+          var type = $(e.currentTarget).attr("data-type");
+          
+          $(e.currentTarget).siblings().removeClass("select").end().addClass("select");
+          
+          $(".opinion-bar, .debates.bottom.debated, .debates.bottom.liked").hide();
+          
+          if (type === "num") {
+            $(".opinion-bar").show();
+          } else {
+            $(".debates.bottom."+type).show();
+          }
+        },
+        
+        render: function (qid,did,reply) {
+          var that = this;
+          
+          this.models.question.url = "http://ec2-107-22-36-240.compute-1.amazonaws.com/api/questions/"+qid;
+          
+          
+          
+        
+        this.models.question.fetch({
+        
+             dataType: "jsonp",
+             
+             success: function(model,questiondata) {
+                
+                that.models.question.data = questiondata;
+                
+                /*that.models.stats.fetch({
+
+                dataType: "jsonp",
+
+                success: function (model, statsdata) {
+                
+                   that.models.stats.data = statsdata;                 
+                   _.templateSettings.variable = "stats";
+                   that.$el.html(_.template(_statsTemplate, that.models));
+                   
+                   
+                }
+                 
+                });*/
+                
+                
+                
+                
+                
+                
+               
+                
+                $.ajax({
+                  url: 'stat.json',
+                  dataType : 'json',
+                  success  : function(statsdata) {                     
+                     that.models.stats.data = statsdata;                 
+                   _.templateSettings.variable = "stats";
+                   //console.log(that.models);
+                   //render number$("#wrapper").width()
+                                   
+                   that.$el.append(_.template(_statsTemplate, that.models));
+                    that.drawNum(statsdata);                    
+                   
+                  },
+                  error : function(e) {
+                    console.log(e);
+                  }
+                })
+               
+               $(window).resize(function() {
+                 that.drawNum(that.models.stats.data);
+               })
+
+          
+             }
+        
+        
+        });
+        
+       
+        }
+    });
+    return CommentsView;
+});
