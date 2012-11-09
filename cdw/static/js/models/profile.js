6e656499094a505e2494216db1f13f06c1a08eba
Yufang Chang <yfc204@nyu.edu>
2012-11-09 01:32:56 -0500

    fixed profile model path

--- cdw/static/js/models/profile.js
+++ cdw/static/js/models/profile.js
@@ -3,7 +3,8 @@ define([
   'backbone'
 ], function(_, Backbone) {
   var ProfileModel = Backbone.Model.extend({
-    urlRoot: '/api/profile/'    
+    urlRoot: '/api/profile'    
   });
   return ProfileModel;
 });
+
