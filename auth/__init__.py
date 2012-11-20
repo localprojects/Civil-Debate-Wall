0f2c4f5700984eea712a242f02e3d64b9d0e4206
Sundar Raman <sundar@localprojects.net>
2012-11-20 12:07:37 -0500

    Explicitly set GMT into cookie expiration

--- auth/__init__.py
+++ auth/__init__.py
@@ -382,7 +382,7 @@ class Auth(object):
             
             resp = current_app.make_response(redirect(redirect_url))  
             # Expire the login cookie
-            yesterday = datetime.date.today() - datetime.timedelta(1)
+            yesterday = (datetime.datetime.utcnow() + datetime.timedelta(-1)).strftime("%a, %d %b %Y GMT")
             resp.set_cookie("login", expires=yesterday)
             
             return resp
