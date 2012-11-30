6d201aef792bc7c180277460ef23058d73d9bde4
Yufang Chang <yfc204@nyu.edu>
2012-11-29 20:44:37 -0500

    handle callback after photo upload

--- cdw/static/js/views/users/photo.js
+++ cdw/static/js/views/users/photo.js
@@ -23,7 +23,8 @@ define([
         
         //http://civildebatewall.s3.amazonaws.com/images/users/50a3272185c5d36f62000000-thumbnail.jpg
         
-        console.log($("#upload_target").html())
+        //var data = JSON.parse($("#upload_target").contents().find("body pre").html());
+        
         $("#done").show();
         
       });
