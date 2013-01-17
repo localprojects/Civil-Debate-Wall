7deaac5b16cf32a5e3382a4be4869a77b8a2d7df
Sundar Raman <sundar@localprojects.net>
2013-01-17 15:44:01 -0500

    Remove the mobile folder (it has been incorporated into static)

--- cdw/mobile/admin_common.js
+++
@@ -1,7 +0,0 @@
-
-if(!window.tools){window.tools={};}
-tools.notImplemented=function(){alert('Not yet implemented');}
-tools.bodyClass=tools.bodyClasses=function(klasses,fn){if(klasses.indexOf(' ')>=0){klasses=klasses.split(/\s+/);}
-$(function(){var $body=$('body');if(klasses.join){if($body.is('.'+klasses.join(',.'))){fn();}}else{if($body.hasClass(klasses)){fn();}}});};$(function(){$('form.delete-form button[type=submit]').click(function(e){if(confirm("Are you sure you want to delete this item? This cannot be undone.")){return true}
-e.preventDefault();});$('form.archive-form button[type=submit]').click(function(e){if(confirm("Are you sure you want to archive this item? This cannot be undone.")){return true}
-e.preventDefault();});});
