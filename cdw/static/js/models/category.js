0b9f94ec9f41a9f3fbeecb344e2d8fd6810c7dda
Sundar Raman <sundar@localprojects.net>
2013-01-27 10:19:27 -0500

    Add full path into list and category APIs

--- cdw/static/js/models/category.js
+++ cdw/static/js/models/category.js
@@ -2,8 +2,10 @@ define([
   'underscore',
   'backbone'
 ], function(_, Backbone) {
+  if (!window.location.origin)
+    window.location.origin = window.location.protocol + "//" + window.location.host;
   var categoryModel = Backbone.Model.extend({
-    url: '/api/questions/categories'
+    url: window.location.origin + '/api/questions/categories'
   });
   return categoryModel;
 });
